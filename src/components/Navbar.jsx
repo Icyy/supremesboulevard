41b39d196bc5adac83107070736c8abcb31cf368
Icyy <adityaauty9@gmail.com>
2025-10-13 21:53:26 +0530

    color change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = ({ onOpenEnquiry }) => {
       sx={{
         bgcolor: isMobile
           ? "rgba(255, 255, 255, 0.09)" // lighter glass effect on mobile
-          : "#23362e10", // dark solid color for desktop
+          : "rgba(255, 255, 255, 0.09)", // dark solid color for desktop
         color: "#fff",
         boxShadow: "0 4px 30px rgba(0,0,0,0.1)",
         backdropFilter: isMobile ? "blur(12px)" : "none",
